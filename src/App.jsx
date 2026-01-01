ebf5bef11abf49be2b3a221e4eb46abdda3bf774
uasr208 <arunsinghutkarsh10@gmail.com>
2026-01-01 19:09:05 +0530

    implented menu section

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Hero from './components/Hero';
 import Cocktails from './components/Cocktails';
 import About from './components/About';
 import Art from './components/Art';
+import Menu from './components/Menu';
 
 
 const App = () => {
@@ -17,6 +18,7 @@ const App = () => {
       <Cocktails />
       <About/>
       <Art/>
+      <Menu/>
     </main>
   )
 }
